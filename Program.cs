123dd66bfd3797bf06442abb35f3f39a943e6bef
rowida7 <rowidaabdelfatah@gmail.com>
2026-09-05 23:31:05 +0300

    Exercise 3: Phone Book

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using Microsoft.VisualBasic;
+using System.Collections.ObjectModel;
 using System.Numerics;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace adv03
@@ -41,38 +44,81 @@ namespace adv03
             #endregion
 
             #region Exercise 2: Leaderboard
-            //Create a leaderboard that automatically sorts players by score.
-            //1.Add: 500 = "Ahmed", 200 = "Sara", 800 = "Ali", 350 = "Mona"
-            
-            Dictionary<int, string> leaderboard = new()
-            {
-                {500, "Ahmed"},
-                {200, "Sara"},
-                {800, "ALi"},
-                {350, "Mona"}
-            };
-            //2.Print all entries(they should be sorted by score automatically)
-            Console.WriteLine("--- Dictionary ---");
-            foreach (KeyValuePair<int, string> player in leaderboard)
-                Console.WriteLine($"{player.Key} = {player.Value}");
-            //3.Access the first key and first value
-            Console.WriteLine("--- First Key and Value ---");
-            Console.WriteLine($"Key:{leaderboard.Keys.First()} - Value:{leaderboard.Values.First()}");
-            //4.Check if score 500 exists
-            Console.WriteLine($"Score 500 Exists? {leaderboard.ContainsKey(500)}");
-            //5.Safely get the player with score 999
-            Console.WriteLine("--- Player 999 ---");
-            bool player999 = leaderboard.TryGetValue(999, out string? value);
-            if (player999)
-                Console.WriteLine(value);
-            else
-                Console.WriteLine("Doesn't Exist");
-            //6.Remove the player with score 200 and print the updated list
-            Console.WriteLine("--- After Removing Score 200 ---");
-            leaderboard.Remove(200);
-            foreach (KeyValuePair<int, string> player in leaderboard)
-                Console.WriteLine($"{player.Key} = {player.Value}");
+            ////Create a leaderboard that automatically sorts players by score.
+            ////1.Add: 500 = "Ahmed", 200 = "Sara", 800 = "Ali", 350 = "Mona"
 
+            //Dictionary<int, string> leaderboard = new()
+            //{
+            //    {500, "Ahmed"},
+            //    {200, "Sara"},
+            //    {800, "ALi"},
+            //    {350, "Mona"}
+            //};
+            ////2.Print all entries(they should be sorted by score automatically)
+            //Console.WriteLine("--- Dictionary ---");
+            //foreach (KeyValuePair<int, string> player in leaderboard)
+            //    Console.WriteLine($"{player.Key} = {player.Value}");
+            ////3.Access the first key and first value
+            //Console.WriteLine("--- First Key and Value ---");
+            //Console.WriteLine($"Key:{leaderboard.Keys.First()} - Value:{leaderboard.Values.First()}");
+            ////4.Check if score 500 exists
+            //Console.WriteLine($"Score 500 Exists? {leaderboard.ContainsKey(500)}");
+            ////5.Safely get the player with score 999
+            //Console.WriteLine("--- Player 999 ---");
+            //bool player999 = leaderboard.TryGetValue(999, out string? value);
+            //if (player999)
+            //    Console.WriteLine(value);
+            //else
+            //    Console.WriteLine("Doesn't Exist");
+            ////6.Remove the player with score 200 and print the updated list
+            //Console.WriteLine("--- After Removing Score 200 ---");
+            //leaderboard.Remove(200);
+            //foreach (KeyValuePair<int, string> player in leaderboard)
+            //    Console.WriteLine($"{player.Key} = {player.Value}");
+
+            #endregion
+
+            #region Exercise 3: Phone Book
+            //Build a phone book application.
+            //1.Create a Collection with 4 contacts(name → phone number)
+            Dictionary<string, string> phoneBook = new(4);
+            //2.Add a new contact using [] syntax (add or update)
+            phoneBook["Ali"] = "0109988004";
+            phoneBook["Ahmed"] = "0100558004";
+            //3.Try adding a duplicate using .Add() — catch the exception and print the error
+            Console.WriteLine("--- Catch The Exception ---");
+            try
+            {
+                phoneBook.Add("Ali", "0109988004");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //4.Try adding a duplicate using .TryAdd() — print whether it succeeded
+            Console.WriteLine("--- TryAdd() ---");
+            if (phoneBook.TryAdd("Ali", "0109988704"))
+                Console.WriteLine("Added Successfully");
+            else
+                Console.WriteLine("Failed to Add");
+            //5.Search for a contact that doesn’t exist
+            Console.WriteLine("--- contact that doesn’t exist ---");
+            Console.WriteLine(phoneBook.ContainsValue("Mohsen"));
+            //6.Get a contact with a fallback of "Not Found"
+            Console.WriteLine("--- Not Found Contact ---");
+            if (phoneBook.TryGetValue("Alaa", out string? value))
+                Console.WriteLine("Found Successfully");
+            else
+                Console.WriteLine("Not Found");
+            //7.Print all Keys on one line, then all Values on another line
+            Console.WriteLine("Keys:");
+            foreach (string key in phoneBook.Keys)
+                Console.Write($"- {key}");
+
+            Console.WriteLine("Vlaues:");
+            foreach (string val in phoneBook.Values)
+                Console.WriteLine($"- {val}");
             #endregion
         }
     }
